b9969fd8c2e5a4718dded0e07fdbbe840681e555
xxegoi <250242742@qq.com>
2019-05-10 17:36:33 +0800

    增加ICMODaily接口

---
+++ src/JIT.Application/JIT/TaskAssignment/ICMODaily/Dtos/ICMODailyCreatDto.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace JIT.JIT.ICMODaily.Dtos
+{
+    public class ICMODailyCreatDto
+    {
+        [Required]
+        public int FMOInterID { get; set; }
+        [Required]
+        public string FMOBillNo { get; set; }
+        public DailyDto[] Dailies { get; set; }
+    }
+}
--- src/JITEF/DIME2Barcode/Entities/ICMODaily.cs
+++ src/JITEF/DIME2Barcode/Entities/ICMODaily.cs
@@ -1,9 +1,11 @@
-﻿using Abp.Domain.Entities;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Abp.Domain.Entities;
 
 namespace JITEF.DIME2Barcode
 {
     public partial class ICMODaily:Entity<string>
     {
+        [Column("FID")]
         public override string Id { get=>FID; set=>FID=value; }
     }
 }
