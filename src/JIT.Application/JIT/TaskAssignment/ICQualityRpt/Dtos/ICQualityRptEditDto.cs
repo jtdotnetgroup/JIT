473c5b0a8a59e52e7ff2b829f9fc79f7ba2ae48b
ZhuoHaiLin <1401367008@qq.com>
2019-05-10 22:20:10 +0800

    提交今天改过的分页代码

---
+++ src/JIT.Application/JIT/TaskAssignment/ICQualityRpt/Dtos/ICQualityRptEditDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using Abp.Application.Services.Dto;
+
+namespace JIT.TaskAssignment.ICQualityRpt.Dtos
+{
+    public class ICQualityRptEditDto : IEntityDto<string>
+    {
+        public string Id { get => FID; set => FID = value; }
+
+        public string FID { get; set; }
+        public int FItemID { get; set; }
+        public Nullable<decimal> FAuxQty { get; set; }
+        public string FRemark { get; set; }
+        public string FNote { get; set; }
+    }
+}
---
+++ src/JITEF/DIME2Barcode/Entities/ICQualityRpt.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Abp.Domain.Entities;
+
+namespace JITEF.DIME2Barcode
+{
+    public partial  class ICQualityRpt:Entity<string>
+    {
+        public override string Id { get=>FID; set=>FID=value; }
+    }
+}
